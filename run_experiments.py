2f414a1f404251e6713d761fdc50dc5efcad1cd2
NoushinA <Noushin.azami@txstate.edu>
2024-04-18 23:19:21 -0500

    reorder

--- run_experiments.py
+++ run_experiments.py
@@ -18,10 +18,10 @@ exe_files = [
 # Function to run comp-run and decomp-run scripts on an exe file
 def run_scripts(exe_file):
     # Run comp-run script
-    subprocess.run(["./comp-run.py","single_inputs", "single_src/bin", exe_file])
+    subprocess.run(["./scripts/comp-run.py","single_inputs", "single_src/bin", exe_file])
     
     # Run decomp-run script
-    subprocess.run(["./decomp-run.py", "single_inputs", "single_src/bin", exe_file])
+    subprocess.run(["./scripts/decomp-run.py", "single_inputs", "single_src/bin", exe_file])
 
 # Iterate over each exe file and run the scripts
 for exe_file in exe_files:
