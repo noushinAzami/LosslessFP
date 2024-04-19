6a473232672a3dcf971148db091cc077990b4368
NoushinA <Noushin.azami@txstate.edu>
2024-04-19 18:57:32 -0500

    minor bug fix

--- run_experiments.py
+++ run_experiments.py
@@ -2,28 +2,13 @@
 import sys
 import subprocess
 
-# List of eight input executable files
-exe_files = [
-    "speed-gpu-compress.exe",
-    "speed-gpu-decompress.exe",
-    "ratio-gpu-compress.exe",
-    "ratio-gpu-decompress.exe",
-    "speed-cpu-compress.exe",
-    "speed-cpu-decompress.exe",
-    "ratio-cpu-compress.exe",
-    "ratio-cpu-decompress.exe"
-]
 
-
-# Function to run comp-run and decomp-run scripts on an exe file
-def run_scripts(exe_file):
-    # Run comp-run script
-    subprocess.run(["./scripts/comp-run.py","single_inputs", "single_src/bin", exe_file])
-    
-    # Run decomp-run script
-    subprocess.run(["./scripts/decomp-run.py", "single_inputs", "single_src/bin", exe_file])
-
-# Iterate over each exe file and run the scripts
-for exe_file in exe_files:
-    run_scripts(exe_file)
+subprocess.run(["./scripts/comp-run.py", "single_inputs", "single_src/bin", "speed-gpu-compress.exe"])
+subprocess.run(["./scripts/decomp-run.py", "single_inputs", "single_src/bin", "speed-gpu-decompress.exe"])
+subprocess.run(["./scripts/comp-run.py", "single_inputs", "single_src/bin", "ratio-gpu-compress.exe"])
+subprocess.run(["./scripts/decomp-run.py", "single_inputs", "single_src/bin", "ratio-gpu-decompress.exe"])
+subprocess.run(["./scripts/comp-run.py", "single_inputs", "single_src/bin", "speed-cpu-compress.exe"])
+subprocess.run(["./scripts/decomp-run.py", "single_inputs", "single_src/bin", "speed-cpu-decompress.exe"])
+subprocess.run(["./scripts/comp-run.py", "single_inputs", "single_src/bin", "ratio-cpu-compress.exe"])
+subprocess.run(["./scripts/decomp-run.py", "single_inputs", "single_src/bin", "ratio-cpu-decompress.exe"])
 
